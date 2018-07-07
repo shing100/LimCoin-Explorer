26c0b1c9c90f10bd6b29b8f6dd0782daa8c52635
Lim Geun <shing100@Naver.com>
2018-07-07 22:50:46 +0900

    Update BlocksContainer.js

--- src/Routes/Blocks/BlocksContainer.js
+++ src/Routes/Blocks/BlocksContainer.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 class BlocksContainer extends Component {
   render() {
-    return <BlocksPresenter />;
+    return <BlocksPresenter {...this.props}/>;
   }
 }
 
